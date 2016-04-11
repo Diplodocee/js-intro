c761b967df8b92abcdf86ded88c0a3b6a9034843
Diplodocee <ludwa.borra@gmail.com>
2016-04-11 18:43:39 -0500

    Update scripts.js

--- 03_paragraph/scripts.js
+++ 03_paragraph/scripts.js
@@ -1,4 +1,4 @@
 document.write("<h1>This is my fight song!</h2>");
-document.write("<p>Take back myt life song!</p>");
+document.write("<p>Take back my life song!</p>");
 document.write("<p>Prove I'm alright so...</p>");
 document.write("<p>o-o-ong!</p>");
